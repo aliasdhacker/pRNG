cf624024ff7bb44f7866323a4025d8bf6e4fc838
Andrew Carr <me@acarr.org>
2022-11-07 17:47:01 -0500

    Update to use values specified in global variables.
    Add some comments to specify which values to use for generation.

--- pRNGIdGenerator.java
+++ pRNGIdGenerator.java
@@ -6,16 +6,25 @@ import java.util.Random;
 public class pRNGIdGenerator {
 
     /**
+     * The count for the total number of IDs to generate while testing.
+     */
+    private static final int TOTAL_NUMBER_OF_TEST_IDS_TO_GENERATE = 100000;
+
+    /**
+     * Character stock for generating ids.  Use any values you deem necessary.
+     */
+    private static final char[] ENGLISH_CHARACTERS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    /**
      * List of random values.
      */
-    private static String[] list = new String[1000000];
+    private static String[] list = new String[TOTAL_NUMBER_OF_TEST_IDS_TO_GENERATE];
 
     /**
      * Counter for duplicates found.
      */
     private static int duplicateCount = 0;
 
-    private static final char[] ENGLISH_CHARACTERS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'W', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     /**
      * Test matching of streams, generate random values and test those.
@@ -25,6 +34,9 @@ public class pRNGIdGenerator {
      * @param args
      */
     public static void main(String[] args) {
+        System.out.println("Starting to generate idsa.");
+        long startTime = System.currentTimeMillis();
+        int idCounter = 0;
         list[0] = "aaaaa";
         String randomId = "aaaaa";
 
@@ -34,11 +46,12 @@ public class pRNGIdGenerator {
 
         System.out.println("Should return false: " + Arrays.stream(list).anyMatch(randomId::equals));
 
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < TOTAL_NUMBER_OF_TEST_IDS_TO_GENERATE; i++) {
             randomId = getRandomAlphanumericString(10);
+            idCounter++;
 
             if (i % 1000 == 0 && i != 0) {
-                System.out.println("1000 more ids generated.  Last id generated: ".concat(randomId));
+                System.out.println("1000 more ids generated. Total generated: [" + idCounter + "]  Last id generated: ".concat(randomId));
                 System.out.println("Total duplicate values generated: " + duplicateCount);
             }
 
@@ -49,6 +62,9 @@ public class pRNGIdGenerator {
 
             list[i] = randomId;
         }
+
+        long totalTime = (System.currentTimeMillis() - startTime) / 1000;
+        System.out.println("Generated [" + idCounter + "] ids in [" + totalTime + "] seconds.");
     }
 
     /**
@@ -90,7 +106,6 @@ public class pRNGIdGenerator {
         long timeForUuidIn100Nanos = seconds * 10000000 + nanos * 100;
         long least12SignificantBitOfTime = (timeForUuidIn100Nanos & 0x000000000000FFFFL) >> 4;
         long version = 1 << 12;
-
         return (timeForUuidIn100Nanos & 0xFFFFFFFFFFFF0000L) + version + get64LeastSignificantBits();
     }
 
@@ -102,7 +117,6 @@ public class pRNGIdGenerator {
     private static long get64LeastSignificantBits() {
         long random63BitLong = new Random().nextLong() & 0x3FFFFFFFFFFFFFFFL;
         long variant3BitFlag = 0x8000000000000000L;
-
         return random63BitLong + variant3BitFlag;
     }
 }
